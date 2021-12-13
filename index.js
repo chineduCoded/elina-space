3aafecf87b556ef13c5fe7904ca58b404866cdf6
chineduCoded <chinedujohn17@gmail.com>
2021-12-13 03:45:40 +0100

    backend

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require("cors")
+
+const products = require("./products")
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Elina's Space API..")
+});
+
+app.get("/products", (req, res) => {
+    res.send(products)
+})
+
+const port = process.env.PORT || 5000
+
+app.listen(port, console.log(`Server is running on port ${port}`))
---
+++ products.js
@@ -0,0 +1,25 @@
+const products = [
+    {
+        id: 1,
+        name: "Sandal Shoe",
+        desc: "Very quality Sandal",
+        price: 699,
+        image: "https://res.cloudinary.com/dtzzqvuzs/image/upload/v1639247916/ElinaShop/shoe5_o5yafo.jpg"
+    },
+    {
+        id: 2,
+        name: "Lace Shoe",
+        desc: "Very quality Lace",
+        price: 456,
+        image: "https://res.cloudinary.com/dtzzqvuzs/image/upload/v1639247924/ElinaShop/shoe4_dujzvx.png"
+    },
+    {
+        id: 3,
+        name: "Shirt",
+        desc: "Quality Shirt",
+        price: 696,
+        image: "https://res.cloudinary.com/dtzzqvuzs/image/upload/v1639301944/ElinaShop/shirt_kyo1s5.jpg"
+    }
+];
+
+module.exports = products;
